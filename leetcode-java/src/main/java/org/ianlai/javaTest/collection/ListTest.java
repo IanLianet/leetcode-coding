cfe753f265d3e8a3511c63ed897b96079ab822d7
ianlianetlai_laptop <ianlianetlai@gmail.com>
2022-08-16 14:53:20 +0800

    [+] Stack & List test added

---
+++ leetcode-java/src/main/java/org/ianlai/javaTest/collection/ListTest.java
@@ -0,0 +1,17 @@
+package org.ianlai.javaTest.collection;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 关于List的小实验
+ *
+ * @author ianlianetlai
+ */
+public class ListTest {
+    public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("123","456");
+
+    }
+}
