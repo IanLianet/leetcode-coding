5e424fafc9af55fb40ab982737c3406d26ffcf7f
赖伊扬 <yiyanglai@digitalgd.com.cn>
2023-02-15 15:29:49 +0800

    [+] leetcode no.0632added

--- leetcode-java/src/main/java/org/ianlai/leetcodejava/official/range700/LC_official_0632.java
+++ leetcode-java/src/main/java/org/ianlai/leetcodejava/official/range700/LC_official_0632.java
@@ -1,5 +1,7 @@
 package org.ianlai.leetcodejava.official.range700;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LC_official_0632 {
@@ -20,15 +22,60 @@ public class LC_official_0632 {
 
      */
     public static void main(String[] args) {
-        System.out.println();
+        List<List<Integer>> nums = new ArrayList<List<Integer>>();
+        nums.add(Arrays.asList(4, 10, 15, 24, 26));
+        nums.add(Arrays.asList(0, 9, 12, 20));
+        nums.add(Arrays.asList(5, 18, 22, 30));
+
+        int[] ints = smallestRange(nums);
+        System.out.println(ints[0] + " " + ints[1]);
     }
 
-    public int[] smallestRange(List<List<Integer>> nums) {
+    public static int[] smallestRange(List<List<Integer>> nums) {
 
+        int n = 0;
+        for (List<Integer> integers : nums) {
+            n += integers.size();
+        }
+        int[][] numGroup = new int[n][2];
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            List<Integer> integers = nums.get(i);
+            for (int j = 0; j < integers.size(); j++) {
+                numGroup[k] = new int[]{integers.get(j), i};
+                k++;
+            }
+        }
+        Arrays.sort(numGroup, (o1, o2) -> (o1[0] - o2[0]));
+//        for (int[] perNumGroup : numGroup) {
+//            System.out.println(perNumGroup[0] + " " + perNumGroup[1]);
+//        }
+        int i = 0;
+        int j;
+        int[] ans = {numGroup[0][0], numGroup[n - 1][0]};
+        int[] groupCount = new int[nums.size()];
+        int totalCount = 0;
+        for (j = 0; j < n; j++) {
+            if (groupCount[numGroup[j][1]]++ == 0) {
+                totalCount++;
+            }
+            if (totalCount == nums.size()) {
+                while (groupCount[numGroup[i][1]] != 1) {
+                    groupCount[numGroup[i][1]]--;
+                    i++;
+                }
+                if ((numGroup[j][0] - numGroup[i][0]) < (ans[1] - ans[0])) {
+                    ans[1] = numGroup[j][0];
+                    ans[0] = numGroup[i][0];
+                }
+                groupCount[numGroup[i][1]]--;
+                i++;
+                totalCount--;
+            }
+        }
 
-        return null;
+        return ans;
     }
-
 
 
 }
